810ca41bf6a885c9d61313247cf4d2bfe1ec3fc8
Petr Horák <petr.horak@tanganica.cz>
2024-12-01 10:50:57 +0100

    Add project files.

---
+++ Day1/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Day 1");
+
+List<string> inputLines = [.. File.ReadAllLines("./input.txt")];
+
+List<int> left = inputLines.Select(x => int.Parse(new string([.. x.TakeWhile(y => !char.IsWhiteSpace(y))]))).Order().ToList();
+List<int> right = inputLines.Select(x => int.Parse(new string([.. x.SkipWhile(y => !char.IsWhiteSpace(y))]))).Order().ToList();
+
+//part 1
+int difsSum = 0;
+for (int i = 0; i < left.Count; i++)
+{
+    difsSum += Math.Abs(left[i] - right[i]);
+}
+Console.WriteLine(difsSum);
+
+//part 2
+int similarityScoresSum = 0;
+Dictionary<int, int> rightDistinctNumsCounts = right.GroupBy(x => x).ToDictionary( x => x.Key, x => x.Count());
+for (int i = 0; i < left.Count; i++)
+{
+    if (rightDistinctNumsCounts.ContainsKey(left[i]))
+    {
+        similarityScoresSum += left[i] * rightDistinctNumsCounts[left[i]];
+    }
+}
+Console.WriteLine(similarityScoresSum);
